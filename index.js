7f6abc3320febbd95b847a783539ffc7d5700066
Stavros Droulias <stavros.d27@gmail.com>
2024-09-07 13:48:03 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'rob';
+function upperCaseCustomerName() {
+return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'toby';
+}
